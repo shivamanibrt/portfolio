98c977710f5930152408423e8cee602a7376af80
Shiv Bartaula <74212245+shivamanibrt@users.noreply.github.com>
2025-03-04 19:58:49 +1100

    shiv name update

--- src/Components/NavigationBarFile.js
+++ src/Components/NavigationBarFile.js
@@ -1,29 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export const NavigationBarFile = ({ isDarkMode, handleThemeChange }) => {
-    const hashnode = 'https://shivamani.hashnode.dev/';
+    const hashnode = "https://shivamani.hashnode.dev/";
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg={isDarkMode ? "dark" : "light"} variant={isDarkMode ? "dark" : "light"}>
-                <Container >
-                    <Navbar.Brand as={Link} to="/" className="navbar-brand-red">
-
-                        Shivamani
+            <Navbar
+                collapseOnSelect
+                expand='lg'
+                bg={isDarkMode ? "dark" : "light"}
+                variant={isDarkMode ? "dark" : "light"}
+            >
+                <Container>
+                    <Navbar.Brand as={Link} to='/' className='navbar-brand-red'>
+                        Shiv
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to="hero">About</Nav.Link>
-                            <Nav.Link as={Link} to="recentWork">Portfolio</Nav.Link>
-                            <Nav.Link as={Link} to="techStack">Skills</Nav.Link>
-                            <Nav.Link as={Link} to="getInTouch">Get-in-touch</Nav.Link>
-                            <Nav.Link as={Link} to="contactUsFile">Contact-Form</Nav.Link>
-                            <Nav.Link href={hashnode} target="_blank">Blog</Nav.Link>
+                    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                    <Navbar.Collapse id='responsive-navbar-nav'>
+                        <Nav className='me-auto'>
+                            <Nav.Link as={Link} to='hero'>
+                                About
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='recentWork'>
+                                Portfolio
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='techStack'>
+                                Skills
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='getInTouch'>
+                                Get-in-touch
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='contactUsFile'>
+                                Contact-Form
+                            </Nav.Link>
+                            <Nav.Link href={hashnode} target='_blank'>
+                                Blog
+                            </Nav.Link>
                         </Nav>
 
                         <Nav>
@@ -33,16 +48,17 @@ export const NavigationBarFile = ({ isDarkMode, handleThemeChange }) => {
                                 </Button>
                             </Nav.Link> */}
 
-                            <Nav.Link onClick={handleThemeChange} >
-                                {isDarkMode
-                                    ? (<i className="fa-solid fa-toggle-on fa-xl"></i>)
-                                    : (<i className="fa-solid fa-toggle-off fa-xl"></i>)
-                                }
+                            <Nav.Link onClick={handleThemeChange}>
+                                {isDarkMode ? (
+                                    <i className='fa-solid fa-toggle-on fa-xl'></i>
+                                ) : (
+                                    <i className='fa-solid fa-toggle-off fa-xl'></i>
+                                )}
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
-            </Navbar >
+            </Navbar>
         </>
     );
 };
